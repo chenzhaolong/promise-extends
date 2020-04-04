0b9a9faad9822edcdaba835176bb1d108ca341aa
chenzhaolong <1332588507@qq.com>
2020-04-04 11:25:22 +0800

    完成finally的一部分单元用例

--- test/finally.test.ts
+++ test/finally.test.ts
@@ -10,24 +10,42 @@ const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
-PromiseExtends.extend({isExtend: true});
+PromiseExtends.extend({isExtend: false});
 
 describe('test Promise.prototype.finally', () => {
+    const api = function(isSuccess: boolean) {
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                isSuccess ? res(1) : rej('error');
+            }, 100);
+        });
+    };
+
     it('loading can be false when success', () => {
         let loading = true;
-        const api = function() {
-            return new Promise((res, rej) => {
-                setTimeout(() => {
-                    res();
-                }, 500);
-            });
-        };
         const handle = () => {
-            return api().finally(() => {
-                loading = false;
-            }).then(() => {
-                return loading;
-            });
+            return api(true)
+                .then(d => d)
+                .finally(() => {
+                    loading = false;
+                }).then(() => {
+                    return loading;
+                });
+        };
+        return expect(handle()).to.become(false);
+    });
+
+    it('loading can be false when fail', () => {
+        let loading = true;
+        const handle = () => {
+            return api(false)
+                .then(d => d)
+                .finally(() => {
+                    loading = false;
+                })
+                .catch(e => {
+                    return loading;
+                })
         };
         return expect(handle()).to.become(false);
     });
